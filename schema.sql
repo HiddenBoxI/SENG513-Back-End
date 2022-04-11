9df3f8e847f3ed3a9dce8122485380e6c0620f04
ninaaaaaaaaaaaaa <ninalsd1111@gmail.com>
2022-04-11 01:04:22 -0600

    update

--- schema.sql
+++ schema.sql
@@ -24,4 +24,6 @@ CREATE TABLE t_user
   COLLATE = utf8mb4_general_ci COMMENT ='User';
 
 insert into t_user (nickname, password, create_at, update_at, totalwins, gamesplayed, winrate, currentstreak, higheststreak, lastplayed)
-values ("lalala","1234",now(),now(),1,1,12.5,1,1,now());
+values ("lalala","1234",now(),now(),1,2,12.5,3,3,now());
+insert into t_user (nickname, password, create_at, update_at, totalwins, gamesplayed, winrate, currentstreak, higheststreak, lastplayed)
+values ("abc","1234",now(),now(),3,9,10.5,2,1,now());
